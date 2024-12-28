6bd89cd525e0e11b73ef81cb4972b4d933de370f
Danaé A.M. <dev.danae@gmail.com>
2024-12-28 13:38:34 +0100

    Test deployApi

--- deployApp/chunk-5X3LRLMY.js
+++
@@ -1,1 +0,0 @@
-var t={production:!1,backendLoginClient:"http://localhost:443/api/utilisateur/login",backendCatalogue:"http://localhost:443/api/catalogue"};export{t as a};
---
+++ deployApp/chunk-ZEYCP35X.js
@@ -0,0 +1,1 @@
+var n={production:!1,backendLoginClient:"http://cnamapp-8h04.onrender.com/api/utilisateur/login",backendCatalogue:"http://cnamapp-8h04.onrender.com/api/catalogue"};export{n as a};
